22c33d5f01fe188a9d4c2c302a0f809e2abedac0
charly.guirao <caferdo@gmail.com>
2014-04-05 16:49:02 -0300

    Initial Project - Ninject and moq added

---
+++ TripShare/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace TripShare.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private readonly IKernel kernel;
+
+        public NinjectControllerFactory()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)kernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            // put bindings here
+        }
+    }
+}
---
+++ TripShare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TripShare.Startup))]
+namespace TripShare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
